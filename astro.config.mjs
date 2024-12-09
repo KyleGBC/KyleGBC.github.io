5e405df68816fa6915fc93ee6ce9b4172ea44691
KyleGBC <kyleroleson@gmail.com>
2024-12-09 06:39:32 -0500

    add takeaways section

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import remarkToc from 'remark-toc';
+import emoji from 'remark-emoji'
 
 // https://astro.build/config
 export default defineConfig({
     markdown: {
-        remarkPlugins: [ [ remarkToc, {heading: 'Contents', maxDepth: 4} ] ]
+        remarkPlugins: [ [ remarkToc, {heading: 'Contents', maxDepth: 4} ], emoji ]
     },
     site: "https://KyleGBC.github.io",
     integrations: [mdx()]
