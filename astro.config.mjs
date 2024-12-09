bdf9fa4de3fe8de4f96897dd4f9e1686b6f42626
KyleGBC <kyleroleson@gmail.com>
2024-12-09 06:00:34 -0500

    Curved ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        remarkPlugins: [ [ remarkToc, {heading: 'Contents', maxDepth: 4} ] ]
+    },
     site: "https://KyleGBC.github.io",
     integrations: [mdx()]
 });
